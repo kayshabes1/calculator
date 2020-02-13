5718b73155e65a87dc29c6b16a0b22702d3fa954
Kaysha Besi <kayshabesi@gmail.com>
2020-02-13 11:16:56 +0300

    divide function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-    var subtract = function(number1, number2){
-    return number1 - number2;
+    var divide = function(number1, number2){
+    return number1 / number2;
     };
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 = parseInt(prompt("Enter another number:"));
-    var result = subtract(number1, number2);
+    var result = divide(number1, number2);
     alert(result);
     
     
